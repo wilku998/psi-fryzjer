cc0f5f8876d1c618ded9cb0c6f1cf51e1fd982f3
wilku998 <wilkbartosz98@wp.pl>
2019-06-16 22:29:03 +0200

    Blazy moved to dashboard comp

--- src/compontents/Dashboard/Dashboard.js
+++ src/compontents/Dashboard/Dashboard.js
@@ -1,14 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
+import Blazy from "blazy";
+
+import Loader from "../Loader/Loader";
 import Header from "./Header/Header";
 import About from "./About/About";
 import Footer from "./Footer/Footer";
 
-const Dashboard = () => (
-  <div>
-    <Header />
-    <About />
-    <Footer />
-  </div>
-);
+let loadedImages = 0;
 
+const Dashboard = () => {
+  loadedImages = 0;
+  const [loaderVisible, setloaderVisible] = useState(true);
+
+  const bLazy = new Blazy({
+    success: img => {
+      loadedImages += 1;
+      setloaderVisible(loadedImages < 11);
+    },
+    offset: 99999999999999999
+  });
+
+  return (
+    <div>
+      {loaderVisible && <Loader />}
+      <Header />
+      <About />
+      <Footer />
+    </div>
+  );
+};
 export default Dashboard;
